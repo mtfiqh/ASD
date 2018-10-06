892fd33234d4faeffcd8d40c4801f02436ee6624
recchan13 <93rezao@gmail.com>
2018-10-06 14:32:54 +0700

    up

---
+++ A.cpp
@@ -0,0 +1,15 @@
+
+/* I.S : L1 dan L2 terdefinisi, L3 kosong */
+/* F.S : Menghasilkan list baru yaitu L3, hasil dari konkatenansi L1 dan L2 */
+void concat(List L1, List L2, List *L3);
+
+/* I.S. : L terdefinisi */
+/* F.S. : mengirimkan hasil penjumlahan seluruh bilangan ganjil yang ada di List L */
+infotype sumGanjil(List L);
+
+
+int main () {
+
+
+}
+
---
+++ B.cpp
@@ -0,0 +1,13 @@
+
+/* I.S. : L terdefinisi */
+/* F.S. : seluruh elemen yang ada di dalam L terurut secara ascending(menaik) */
+void selectionSort(List *L){
+	address P1 = First(*L);
+	infotype temp;
+}
+
+int main () {
+
+}
+
+
